0e17e07c8f5804d07f96fd5f876fc9e58bc10d4d
playerx015 <playerx015@gmail.com>
2022-05-24 23:43:39 +0200

    sa abs mapom

--- src/components/Stranica.jsx
+++ src/components/Stranica.jsx
@@ -1,5 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import {useParams, Link} from 'react-router-dom'
+import {MapContainer, TileLayer} from 'react-leaflet'
+
+import "leaflet/dist/leaflet.css";
+import '../index.css';
 
 const Stranica = () => {
     const params = useParams();
@@ -12,13 +16,26 @@ const Stranica = () => {
 
     return (
         <>
+        <div style={{position: "relative"}}>
+                            <div style={{position: "absolute", top: 65, left: 10}}>
+                                <MapContainer zoom={15} center={{lat:44.473294 , lng:20.568483}}>
+                                <TileLayer
+                                    url="https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=xTAI4G9nYmojQ7AVVFx4"
+
+                                    />
+                                </MapContainer>
+                            </div>
+                         </div>
+
             {!lokacija && <div style = {{marginTop : "50px"}} >Loading</div>}
             {!!lokacija && (
                 <div style = {{marginTop : "50px", width: "100%"}}>
                         <h2 style = {{textAlign : "center"}}>{lokacija.ime}</h2> 
+
                         <div style={{margin: "auto", width: "40%"}}>
                             <img src ={ `http://localhost:3005/slike/${lokacija.slika}`} width="100%" />
                         </div>
+                        
                         <div style={{margin: "auto", width: "50%"}}>
                             {lokacija.tekst}
                         </div>
